f5a42f5d3760e5ea4afad97412fed5f4b7518f0f
Joanna Kao <joanna.kao@ft.com>
2016-08-18 13:55:06 -0400

    create one script for generating line charts with configs

--- client/scripts/drawCharts.js
+++ client/scripts/drawCharts.js
@@ -1,23 +1,85 @@
-/* global document, drawBondData, drawLifeExpectancies, loadComparisonInteractive */
+/* global document */
 
 import * as d3 from 'd3';
-import { drawBondData } from './drawBondData';
-import { drawLifeExpectancies } from './drawLifeExpectancies';
+import { drawLineChart } from './drawLineChart';
 import { loadComparisonInteractive } from './loadComparisonInteractive';
 
 let windowWidth = window.innerWidth;
 
+const config = {
+  bondYields: {
+    dateFormat: '%d-%b-%Y',
+    selectorId: 'interest-rates-chart',
+    yAxisLabel: '%',
+    yScale: [-1, 16],
+    xDomain: ['01-Jan-1980', '31-Dec-2016'],
+    columns: [
+      {
+        columnName: 'GUKG10 Index',
+        color: '#76acb8',
+        label: 'UK 10-year gilt yield',
+        labelPlacement: { x: '01-Jan-1991', y: 12.5 },
+        strokeWeight: 1,
+      },
+      {
+        columnName: 'GDBR10 Index',
+        color: '#3d7ab3',
+        label: 'German bund yield',
+        labelPlacement: { x: '01-Jan-1990', y: 4.2 },
+        strokeWeight: 1,
+      },
+      {
+        columnName: 'USGG10YR Index',
+        color: '#af516c',
+        label: 'US 10-year Treasury yield',
+        labelPlacement: { x: '01-Jan-1980', y: 16.7 },
+        strokeWeight: 2,
+      },
+    ],
+  },
+  lifeExpectancy: {
+    dateFormat: '%Y',
+    selectorId: 'life-expectancies-chart',
+    yAxisLabel: '%',
+    yScale: [68, 80],
+    xDomain: [1960, 2013],    
+    columns: [
+      {
+        columnName: 'United Kingdom',
+        color: '#76acb8',
+        label: 'UK',
+        labelPlacement: { x: 2009, y: 81 },
+        strokeWeight: 1,
+      },
+      {
+        columnName: 'Germany',
+        color: '#3d7ab3',
+        label: 'Germany',
+        labelPlacement: { x: 2008, y: 79.2 },
+        strokeWeight: 1,
+      },
+      {
+        columnName: 'United States',
+        color: '#af516c',
+        label: 'US',
+        labelPlacement: { x: 2011, y: 77.8 },
+        strokeWeight: 2,
+      },
+    ],
+  },
+};
+
 export function drawCharts(error, bondData, lifeExpectancyData, comparisonData) {
-  drawBondData(bondData);
-  drawLifeExpectancies(lifeExpectancyData);
+  drawLineChart(bondData, config.bondYields, windowWidth);
+  drawLineChart(lifeExpectancyData, config.lifeExpectancy, windowWidth);
   loadComparisonInteractive(comparisonData);
 
   d3.select(window).on('resize', () => {
     if (Math.abs(window.innerWidth - windowWidth) > 10) {
       windowWidth = window.innerWidth;
 
-      drawBondData(bondData, windowWidth);
-      drawLifeExpectancies(lifeExpectancyData, windowWidth);
+      drawLineChart(bondData, config.bondYields, windowWidth);
+      drawLineChart(lifeExpectancyData, config.lifeExpectancy, windowWidth);
     }
   });
 }
@@ -33,7 +95,7 @@ document.getElementById('tweet').addEventListener('click', () => {
   console.log(text);
 
   const tweetText = `&text=${text}`;
-  const related = '&related=ajam';
+  const related = '&related=ft';
   const counterURL = `&counturl=${window.location.hostname + window.location.pathname}`;
 
   const twitterURL = percentEncode(baseURL + tweetText + related + counterURL);
---
+++ client/scripts/drawLineChart.js
@@ -0,0 +1,102 @@
+import * as d3 from 'd3';
+
+export function drawLineChart(data, config, windowWidth) {
+  const parseDate = d3.timeParse(config.dateFormat);
+
+  data.forEach((d) => {
+    d.date = parseDate(d.Date);
+  });
+
+  const chartContainer = d3.select(`#${config.selectorId}`);
+  chartContainer.html('');
+
+  const graphWidth = chartContainer.node().offsetWidth;
+  const graphHeight = 400;
+  const margins = { top: 50, bottom: 50, left: 25, right: 5 };
+
+  chartContainer.append('svg');
+
+  const svg = chartContainer.select('svg');
+
+  svg
+    .attr('width', graphWidth)
+    .attr('height', graphHeight);
+
+  const yScale = d3.scaleLinear()
+    .domain(config.yScale)
+    .range([graphHeight - margins.top - margins.bottom, 0]);
+
+  const yAxis = d3.axisRight()
+    .scale(yScale)
+    .tickSizeInner(graphWidth)
+    .tickSizeOuter(0);
+
+  const yLabel = svg.append('g')
+    .attr('class', 'yAxis')
+    .attr('transform', `translate(0,${margins.top})`)
+    .call(yAxis);
+
+  yLabel.selectAll('.tick text')
+    .attr('x', graphWidth - margins.right - 12)
+    .attr('y', -10);
+
+  const yAxisLabel = yLabel.append('text')
+    .text(config.yAxisLabel)
+    .style('text-anchor', 'right')
+    .attr('class', 'axisLabel')
+    .attr('x', graphWidth - margins.left - margins.right + 12)
+    .attr('y', -32);
+
+  const xScale = d3.scaleTime()
+    .domain([parseDate(config.xDomain[0]), parseDate(config.xDomain[1])])
+    .range([0, graphWidth - margins.left - margins.right]);
+
+  let numXTicks = 10;
+  if (windowWidth < 400) {
+    numXTicks = 2;
+  }
+
+  const xAxis = d3.axisBottom()
+    .scale(xScale)
+    .tickSizeOuter(5)
+    .ticks(numXTicks)
+    .tickFormat(d => d3.timeFormat('%Y')(d));
+
+  const xLabel = svg.append('g')
+    .attr('class', 'xAxis')
+    .attr('transform', `translate(0,${graphHeight - margins.bottom})`)
+    .call(xAxis);
+
+  const lineGroup = svg.append('g')
+    .attr('class', 'lineGroup')
+    .attr('transform', `translate(0,${margins.top})`);
+
+  const annotationGroup = svg.append('g')
+    .attr('class', 'annotationGroup')
+    .attr('transform', `translate(0,${margins.top})`);
+
+  for (let i = 0; i < config.columns.length; i++) {
+    const columnConfig = config.columns[i];
+
+    const lineData = d3.line()
+      .x((d) => xScale(d.date))
+      .y((d) => yScale(+d[columnConfig.columnName]))
+      .defined((d) => d[columnConfig.columnName] !== '');
+
+    const line = lineGroup
+      .datum(data)
+      .append('path')
+      .attr('class', `${columnConfig.columnName}-line`)
+      .attr('d', (d) => lineData(d))
+      .style('stroke', columnConfig.color)
+      .style('stroke-width', columnConfig.strokeWeight)
+      .style('fill', 'none');
+
+    annotationGroup.append('text')
+      .attr('class', 'annotationLabel')
+      .attr('x', xScale(parseDate(columnConfig.labelPlacement.x)))
+      .attr('y', yScale(columnConfig.labelPlacement.y))
+      .style('fill', columnConfig.color)
+      .text(columnConfig.label);
+  }
+}

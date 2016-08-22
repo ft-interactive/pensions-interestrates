dfcba14a9eae413d5a5c4b82431dc94777424112
Joanna Kao <joanna.kao@ft.com>
2016-08-22 12:09:48 -0400

    change times smaller to per cent smaller

--- client/scripts/loadComparisonInteractive.js
+++ client/scripts/loadComparisonInteractive.js
@@ -20,7 +20,12 @@ function changeTweetText() {
   const publicPrivate = $('#publicprivate .interactive-option[aria-pressed=true]').text();
   const companyCountry = $('#interactive-compare').val();
   const result = $('#interactive-result').text();
-  const multiplier = result.replace(/,/g, '').match(/\d+/)[0];
+  let multiplier;
+  if (result.indexOf('smaller') >= 0) {
+    multiplier = Math.round(100 / (100 - (result.replace(/,/g, '').match(/\d+/)[0])));
+  } else {
+    multiplier = result.replace(/,/g, '').match(/\d+/)[0];
+  }
 
   let unit = '';
   if (['Afghanistan', 'Angola', 'Albania', 'Arab World', 'United Arab Emirates', 'Armenia', 'Antigua and Barbuda', 'Australia', 'Austria', 'Azerbaijan', 'Burundi', 'Belgium', 'Benin', 'Burkina Faso', 'Bangladesh', 'Bulgaria', 'Bahrain', 'Bahamas, The', 'Bosnia and Herzegovina', 'Belarus', 'Belize', 'Bolivia', 'Brazil', 'Barbados', 'Brunei Darussalam', 'Bhutan', 'Botswana', 'Central African Republic', 'Canada', 'Central Europe and the Baltics', 'Switzerland', 'Chile', 'China', 'Cote d\'Ivoire', 'Cameroon', 'Congo, Rep.', 'Colombia', 'Cabo Verde', 'Costa Rica', 'Caribbean small states', 'Cyprus', 'Czech Republic', 'Germany', 'Dominica', 'Denmark', 'Dominican Republic', 'Algeria', 'East Asia & Pacific (excluding high income)', 'Early-demographic dividend', 'East Asia & Pacific', 'Europe & Central Asia (excluding high income)', 'Europe & Central Asia', 'Ecuador', 'Egypt, Arab Rep.', 'the Euro area', 'Spain', 'Estonia', 'Ethiopia', 'the European Union', 'Fragile and conflict affected situations', 'Finland', 'Fiji', 'France', 'Gabon', 'the United Kingdom', 'Georgia', 'Ghana', 'Guinea', 'Guinea-Bissau', 'Equatorial Guinea', 'Greece', 'Grenada', 'Guatemala', 'Guyana', 'High income', 'Hong Kong SAR, China', 'Honduras', 'Heavily indebted poor countries (HIPC)', 'Croatia', 'Haiti', 'Hungary', 'IBRD only', 'IDA & IBRD total', 'IDA total', 'IDA blend', 'Indonesia', 'IDA only', 'India', 'Ireland', 'Iraq', 'Iceland', 'Israel', 'Italy', 'Jamaica', 'Jordan', 'Japan', 'Kazakhstan', 'Kenya', 'Kyrgyz Republic', 'Cambodia', 'Kiribati', 'St. Kitts and Nevis', 'Korea, Rep.', 'Kosovo', 'Kuwait', 'Latin America & Caribbean (excluding high income)', 'Lao PDR', 'Lebanon', 'Liberia', 'Libya', 'St. Lucia', 'Latin America & Caribbean', 'Least developed countries: UN classification', 'Low income', 'Sri Lanka', 'Lower middle income', 'Low & middle income', 'Late-demographic dividend', 'Lithuania', 'Luxembourg', 'Latvia', 'Macao SAR, China', 'Morocco', 'Moldova', 'Madagascar', 'Maldives', 'the Middle East & North Africa', 'Mexico', 'Middle income', 'Macedonia, FYR', 'Mali', 'Myanmar', 'Montenegro', 'Mongolia', 'Mozambique', 'Mauritius', 'Malawi', 'Malaysia', 'North America', 'Namibia', 'Niger', 'Nigeria', 'Nicaragua', 'Netherlands', 'Norway', 'Nepal', 'New Zealand', 'OECD members', 'Oman', 'Other small states', 'Pakistan', 'Panama', 'Peru', 'Philippines', 'Palau', 'Poland', 'Pre-demographic dividend', 'Portugal', 'Paraguay', 'Pacific island small states', 'Post-demographic dividend', 'Qatar', 'Romania', 'the Russian Federation', 'Rwanda', 'South Asia', 'Saudi Arabia', 'Sudan', 'Senegal', 'Singapore', 'the Solomon Islands', 'Sierra Leone', 'El Salvador', 'Somalia', 'Serbia', 'Sub-Saharan Africa (excluding high income)', 'South Sudan', 'Sub-Saharan Africa', 'Small states', 'Suriname', 'the Slovak Republic', 'Slovenia', 'Sweden', 'Swaziland', 'Seychelles', 'Chad', 'East Asia & Pacific (IDA & IBRD countries)', 'Europe & Central Asia (IDA & IBRD countries)', 'Togo', 'Thailand', 'Tajikistan', 'Turkmenistan', 'Latin America & the Caribbean (IDA & IBRD countries)', 'Timor-Leste', 'South Asia (IDA & IBRD)', 'Sub-Saharan Africa (IDA & IBRD countries)', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Tanzania', 'Uganda', 'Ukraine', 'Upper middle income', 'Uruguay', 'the United States', 'Uzbekistan', 'St. Vincent and the Grenadines', 'Vietnam', 'West Bank and Gaza', 'World', 'Samoa', 'South Africa', 'Congo, Dem. Rep.', 'Zambia', 'Zimbabwe'].indexOf(companyCountry) > -1) {
@@ -44,6 +49,8 @@ function changeTweetText() {
   let availableSpace;
 
   let sentence = `The ${usUk} ${publicPrivate} pension deficit (${deficitVal}) is ${result} than ${companyCountry}${unit}`;
+
+  sentence = sentence.replace(/%/g, '%25');
 
   if (multiplier <= 7 && multiplier >= 2) { // if too long, will overflow into two lines.
     let barChart = '%0D%0A';
@@ -118,7 +125,8 @@ export function loadComparisonInteractive(data) {
         multiplyFactor = Math.round(100 / multiplyFactor) / 100;
       }
       multiplyFactor = numberWithCommas(multiplyFactor);
-      interactiveText = `${multiplyFactor} times smaller`;
+      const percentNum = 100 - Math.round(100 / multiplyFactor);
+      interactiveText = `${percentNum}% smaller`;
     } else {
       if (Math.round(1 / multiplyFactor) !== 1) {
         multiplyFactor = numberWithCommas(Math.round(multiplyFactor));

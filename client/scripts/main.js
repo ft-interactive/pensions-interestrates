eb1a204ac99e804c4ef660743bab71a788005ab7
Kavanagh, Luke <ltkavanagh@gmail.com>
2016-08-23 20:53:06 +0100

    Add babel-polyfill
    This is an ugly fix. babel-polyfill is big and it’s not all needed. I’d
    rather we strip it back to what’s needed. Perhaps one to fix in the
    starter-kit rather than this time around.
    
    Also bump some dependencies.

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,3 +1,5 @@
+import 'babel-polyfill';
+
 import { drawCharts } from './drawCharts';
 import { queue } from 'd3-queue';
 import { tsv } from 'd3';
@@ -9,5 +11,4 @@ queue()
   .defer(tsv, 'data/comparisonData.tsv')
   .await(drawCharts);
 
-// YOUR CODE HERE
 document.dispatchEvent(new CustomEvent('ig.Loaded'));

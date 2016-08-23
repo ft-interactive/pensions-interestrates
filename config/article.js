3da20b600b228b510ea9d0feee7473dd001313a2
Joanna Kao <joanna.kao@ft.com>
2016-08-22 20:03:21 -0400

    fix social text

--- config/article.js
+++ config/article.js
@@ -5,7 +5,7 @@ export default _ => ({ // eslint-disable-line
 
   // canonical URL of the published page
   // https://ig.ft.com/sites/pensions-interestrates get filled in by the ./configure script
-  url: 'https://ig.ft.com/sites/pensions-interestrates-explainer',
+  url: 'https://ig.ft.com/sites/pensions-interestrates-explainer/',
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')
@@ -50,7 +50,7 @@ export default _ => ({ // eslint-disable-line
 
   // optional social meta data
   // twitterCreator: '@individual's_account',
-  socialHeadline:  'Pensions\' painful arithmetic: Bond mathematics and the scale of pension deficits explained',
+  socialHeadline:  'Pensions\' painful arithmetic',
   socialSummary:  'Bond mathematics and the scale of pension deficits explained',
 
   onwardjourney: {
